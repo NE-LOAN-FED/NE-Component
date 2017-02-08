24e45fb4f91160a25e5fdc388799daef559a048b
limerickgds <guods2015@gmail.com>
2017-02-08 15:12:07 +0800

    fix: add all components

--- components/index.js
+++ components/index.js
@@ -2,4 +2,29 @@
  * Created by hzyuanqi1 on 2016/10/8.
  */
 
-export * from './Alert/index'
+export { default as Alert } from './Alert'
+export { default as Button } from './Button'
+export {
+  Cell,
+  CellBody,
+  CellHeader,
+  CellFooter,
+  CellTip,
+  Cells
+} from './Cell'
+export { default as CellInput } from './CellInput'
+export { default as DatePicker } from './DatePicker'
+export {
+  Form,
+  Input,
+  CheckBox,
+  Select,
+  FormCell
+} from './Form'
+export { default as Icon } from './Icon'
+export { default as Modal } from './Modal'
+export { default as Panel } from './Panel'
+export { default as Picker } from './Picker'
+export { default as Toast } from './Toast'
+export { default as VerifyButton } from './VerifyButton'
+
