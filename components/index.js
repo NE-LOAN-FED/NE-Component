81f8bed18b5a443caf357eaf20aef3c5f112a603
kisnows <yq12315@gmail.com>
2017-02-14 13:49:48 +0800

    修正样式引用错误

--- components/index.js
+++ components/index.js
@@ -1,9 +1,9 @@
 /**
  * Created by hzyuanqi1 on 2016/10/8.
  */
-
-export { default as Alert } from './Alert'
-export { default as Button } from './Button'
+import './style/index.scss'
+export {default as Alert} from './Alert'
+export {default as Button} from './Button'
 export {
   Cell,
   CellBody,
@@ -12,8 +12,8 @@ export {
   CellTip,
   Cells
 } from './Cell'
-export { default as CellInput } from './CellInput'
-export { default as DatePicker } from './DatePicker'
+export {default as CellInput} from './CellInput'
+export {default as DatePicker} from './DatePicker'
 export {
   Form,
   Input,
@@ -21,10 +21,10 @@ export {
   Select,
   FormCell
 } from './Form'
-export { default as Icon } from './Icon'
-export { default as Modal } from './Modal'
-export { default as Panel } from './Panel'
-export { default as Picker } from './Picker'
-export { default as Toast } from './Toast'
-export { default as VerifyButton } from './VerifyButton'
+export {default as Icon} from './Icon'
+export {default as Modal} from './Modal'
+export {default as Panel} from './Panel'
+export {default as Picker} from './Picker'
+export {default as Toast} from './Toast'
+export {default as VerifyButton} from './VerifyButton'
 
