cab65e9d1ebd38e9630ee0968c424a0646cdcd96
yanm1ng <yummy1121@foxmail.com>
2017-06-28 14:11:23 +0800

    ActionSheet组件的菜单点击，同时添加取消菜单

--- src/ActionSheet/ActionSheet.js
+++ src/ActionSheet/ActionSheet.js
@@ -10,10 +10,13 @@ const noop = () => { }
 export default class ActionSheet extends React.Component {
   static propTypes = {
     prefixCls: PropTypes.string,
-    show: PropTypes.bool,       // modal 显示
-    onClose: PropTypes.func,  // modal close 事件
-    items: PropTypes.array,   // 内容列表
+    show: PropTypes.bool,       // 显示
+    onMaskClick: PropTypes.func,  // 遮罩点击事件
+    menus: PropTypes.array,   // 内容列表
+    onMenuChange: PropTypes.func, // 选项点击事件
     autoClose: PropTypes.bool,  // 点击一个选项后，是否自动关闭
+    showCancel: PropTypes.bool,  // 显示底部取消
+    cancelText: PropTypes.string, // 取消文本
     transitionName: PropTypes.string, // 动画的类名
     transitionTimeOut: PropTypes.number // 动画的时间
   }
@@ -21,27 +24,59 @@ export default class ActionSheet extends React.Component {
   static defaultProps = {
     prefixCls: 'NEUI',
     show: false,
-    onClose: noop,
+    onMaskClick: noop,
+    onMenuChange: noop,
     autoClose: true,
+    showCancel: false,
+    cancelText: '取消',
     transitionName: 'verticalSlideBT',
     transitionTimeOut: 300,
-    items: []
+    menus: []
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      open: props.show || false
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      open: props.show
+    })
+  }
+
+  onMenuClick = (key) => {
+    const { autoClose, onMenuChange } = this.props
+    if (autoClose) {
+      this.setState({
+        open: false
+      })
+    }
+    onMenuChange && onMenuChange(key)
   }
 
   render () {
     const {
-      prefixCls, items, className, show, onClose, autoClose, transitionName, transitionTimeOut, ...others
+      prefixCls, menus, className, showCancel, cancelText, onMaskClick, transitionName, transitionTimeOut, ...others
     } = this.props
+    const {
+      open
+    } = this.state
     const cls = classname({
       [`${prefixCls}_action__sheet`]: true,
       [className]: className
     })
     return (
-      <Modal show={show} transitionName={transitionName} transitionTimeOut={transitionTimeOut} onClickAway={onClose}>
-        <ul className={cls} {...others}>
-          {items.map((el, index) => {
-            return <li key={index}>{el}</li>
+      <Modal show={open} transitionName={transitionName} transitionTimeOut={transitionTimeOut} onClickAway={onMaskClick}>
+        <ul className={cls}>
+          {menus.map((el, index) => {
+            return <li key={index} onClick={() => this.onMenuClick(index)}>{el}</li>
           })}
+          {showCancel &&
+          <li className={`${prefixCls}_action_cancel`} key={-1} onClick={() => this.onMenuClick(-1)}>{cancelText}</li>
+          }
         </ul>
       </Modal>
     )
